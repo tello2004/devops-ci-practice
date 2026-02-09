f5de8b07f95132ad67f68440633ff6f1bc81c2cb
Tony-23-glez <23gonzalezca@gmail.comgit config --global user.email 23gonzalezca@gmail.com>
2026-02-09 08:34:44 -0600

    Actualizacion de Asignacion de variables

---
+++ resta.py
@@ -0,0 +1,6 @@
+#Asignamos a las variables a y b los valores ingresados por el usuario, 
+# convirtiendolos a tipo float para permitir decimales.
+a = float(input("Ingrese el primer numero: "))
+b = float(input("Ingrese el segundo numero: "))
+
+#Falta hacer la resta de a y b, y almacenar el resultado en la variable resultado.
