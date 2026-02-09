7e2d17d99e4a40598451c82285e0e8da5cae2b7b
Angel <hu220112814@lahuerta.tecmm.edu.mx>
2026-02-09 08:39:16 -0600

    Resultado de la resta

--- resta.py
+++ resta.py
@@ -4,3 +4,6 @@ a = float(input("Ingrese el primer numero: "))
 b = float(input("Ingrese el segundo numero: "))
 
 #Falta hacer la resta de a y b, y almacenar el resultado en la variable resultado.
+resultado = a - b
+#Imprimimos el resultado de la resta.
+print("El resultado de la resta es: ", resultado)
